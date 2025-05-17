2aa3920f75e9125f092d9fd12ecb452117d902da
Lev <vinnipyx38@gmail.com>
2025-05-17 15:11:30 +0500

    Add utils

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, private, users, api_keys
+from app.api.routes import items, login, private, users, api_keys, utils
 from app.core.config import settings
 
 api_router = APIRouter()
+
+api_router.include_router(utils.router)
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(items.router)
